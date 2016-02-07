44784f89d0da542a6c4c96bb663746af830f4803
darkspive <qfybafcc@yandex.ru>
2016-02-07 16:45:38 +0300

    Update main.php

--- src/php/main.php
+++ src/php/main.php
@@ -1,3 +1,54 @@
 <?php
 
+namespace php;
+
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\entity\Entity;
+use pocketmine\event\block\SignChangeEvent;
+use pocketmine\event\entity\EntityDamageByEntityEvent;
+use pocketmine\event\entity\EntityDamageEvent;
+use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerDeathEvent;
+use pocketmine\event\player\PlayerInteractEvent;
+use pocketmine\event\player\PlayerJoinEvent;
+use pocketmine\event\player\PlayerLoginEvent;
+use pocketmine\event\player\PlayerMoveEvent;
+use pocketmine\event\player\PlayerQuitEvent;
+use pocketmine\item\Item;
+use pocketmine\level\particle\FlameParticle;
+use pocketmine\level\particle\DustParticle;
+use pocketmine\level\particle\HeartParticle;
+use pocketmine\level\particle\LavaParticle;
+use pocketmine\level\particle\PortalParticle;
+use pocketmine\level\sound\ClickSound;
+use pocketmine\level\sound\AnvilUseSound;
+use pocketmine\level\sound\BatSound;
+use pocketmine\math\Vector3;
+use pocketmine\Player;
+use pocketmine\plugin\PluginBase;
+use pocketmine\tile\Sign;
+use pocketmine\utils\TextFormat;
+use pocketmine\utils\TextFormat as F;
+use pocketmine\level\particle\AngryVillagerParticle;
+use pocketmine\level\particle\WaterDripParticle;
+use pocketmine\entity\Effect;
+use pocketmine\block\Block;
+use pocketmine\event\player\PlayerDropItemEvent;
+use pocketmine\level\particle\ExplodeParticle;
+use pocketmine\utils\Config;
+use pocketmine\item\enchantment\Enchantment;
+use pocketmine\event\player\PlayerChatEvent;
+use pocketmine\level\particle\ItemBreakParticle;
+use pocketmine\event\entity\EntityRegainHealthEvent;
+use pocketmine\event\player\PlayerRespawnEvent;
+use pocketmine\event\block\BlockBreakEvent;
+use pocketmine\level\particle\FloatingTextParticle;
+use pocketmine\event\player\PlayerCommandPreprocessEvent;
+use pocketmine\event\player\PlayerToggleSprintEvent;
+use pocketmine\event\player\PlayerPreLoginEvent;
+
+class main extends PluginBase implements Listener {
+}
+
 ?>
